a7cbe803129f20a0ad665bc964371380a81326f1
Kelby Ludwig <kelbyludwig@Kelbys-MacBook-Pro-2.local>
2015-06-04 17:17:44 -0500

    Finished 14

--- src/AES.hs
+++ src/AES.hs
@@ -6,7 +6,6 @@ import qualified System.Random as R
 import qualified Crypto.Cipher.AES as AES
 import qualified Data.ByteString as B
 import qualified Crypto as C
-import qualified Encoding as E
 
 
 data AESMode = ECB | CBC deriving (Show)
@@ -32,7 +31,7 @@ ecbChosenPrefix oracle = ecbChosenPrefix' oracle ctlen pad numblocks B.empty
 
 ecbChosenPrefix' :: (B.ByteString -> B.ByteString) -> Int -> B.ByteString -> Int -> B.ByteString -> B.ByteString
 ecbChosenPrefix' _      0 _   _         _     = B.empty
-ecbChosenPrefix' oracle n pad numblocks known = B.append byte (ecbChosenPrefix' oracle (n-1) (B.tail pad) numblocks (B.append known byte)) 
+ecbChosenPrefix' oracle n pad numblocks known = if pad == B.empty then B.empty else B.append byte (ecbChosenPrefix' oracle (n-1) (B.tail pad) numblocks (B.append known byte)) 
                                                  where letters    = map B.singleton [0..255]
                                                        filler     = B.append (B.tail pad) known
                                                        chosenpts  = map (B.append filler) letters
@@ -43,17 +42,22 @@ ecbChosenPrefix' oracle n pad numblocks known = B.append byte (ecbChosenPrefix' 
                                                                         (Just ind) -> letters !! ind
                                                                         Nothing    -> B.singleton 42                
 
---ecbRandomPrefixDecrypt :: (B.ByteString -> B.ByteString) -> B.ByteString
-ecbRandomPrefixDecrypt oracle = ecbRandomPrefixDecrypt' oracle ctlen pad numblocks B.empty
-                                   where ct     = oracle B.empty
-                                         ctlen  = B.length ct
-                                         pad    = B.replicate ctlen 0
-                                         numblocks = div ctlen 16
-
-ecbRandomPrefixDecrypt' :: (B.ByteString -> B.ByteString) -> Int -> B.ByteString -> Int -> B.ByteString -> Int
-ecbRandomPrefixDecrypt' oracle n pad numblocks known = numberOfRepeatBlocks (oracle $ E.toBytes "YELLOW SUBMARINE")
-                                                        where soManyAs             = iterate (B.append $ E.toBytes "A") (E.toBytes "A")
-                                                              numberOfRepeatBlocks = \x -> let b = C.createBlocks x 16 in (length b) - (length $ nub b)
+ecbRandomPrefixDecrypt :: (B.ByteString -> B.ByteString) -> B.ByteString
+ecbRandomPrefixDecrypt oracle = ecbChosenPrefix' oracle' (B.length secretPlusPrefixBytes) pad numberOfNULBlocks B.empty 
+                                 where secretPlusPrefixBytes  = oracle B.empty
+                                       soManyNULs             = iterate (B.append $ B.singleton 0) (B.singleton 0)
+                                       repeatBlocks           = \x -> let b = C.createBlocks x 16 in ((length b) - (length $ nub b))+1
+                                       numberOfSecretBytes    = B.length secretPlusPrefixBytes
+                                       numberOfSecretBlocks   = div numberOfSecretBytes 16
+                                       paddingLength          = 1 + (length $ takeWhile (/= numberOfSecretBlocks) $ map (\x -> repeatBlocks $ oracle x) soManyNULs)
+                                       pad                    = B.replicate paddingLength 0
+                                       blocks                 = C.createBlocks (oracle pad) 16
+                                       dropPrefixBlocks       = B.concat $ concat $ dropPrefix blocks
+                                       numberOfDroppedBytes   = (B.length $ oracle pad) - (B.length dropPrefixBlocks)
+                                       numberOfNULBlocks      = repeatBlocks $ oracle pad
+                                       oracle'                = \x -> B.drop numberOfDroppedBytes $ oracle x   
+dropPrefix :: [B.ByteString] -> [[B.ByteString]]
+dropPrefix list = dropWhile (\x -> length x == 1) $ group list                                                               
 
 --CBC related functions
 cbcEncrypt :: B.ByteString -> B.ByteString -> B.ByteString -> B.ByteString
@@ -102,9 +106,9 @@ createECBOracleRandomPrefix  hidden = do
                                          g <- R.newStdGen
                                          let key = B.pack $ Prelude.take 16 (R.randomRs (W8._nul, 255) g)
                                          g2 <- R.newStdGen
-                                         let length = head $ take 1 (R.randomRs (0, 64) g2)
+                                         let len = head $ take 1 (R.randomRs (0, 64) g2)
                                          g3 <- R.newStdGen
-                                         let prefix = B.pack $ take length (R.randomRs (W8._nul, 255) g3)
+                                         let prefix = B.pack $ take len (R.randomRs (W8._nul, 255) g3)
                                          let oracle = (\input -> ecbEncrypt key $ pkcs7 (B.concat [prefix, input, hidden]) 16)
                                          return oracle
 

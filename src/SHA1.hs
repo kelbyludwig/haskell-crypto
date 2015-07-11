95c13dc5d5fa613693faa444e212670167c233af
Kelby Ludwig <kelbyludwig@No-Name.local>
2015-07-10 21:47:33 -0500

    Need to find a better packing/unpacking methodology.

--- src/SHA1.hs
+++ src/SHA1.hs
@@ -4,10 +4,11 @@ import Data.ByteString as B
 import Data.ByteString.Builder as BLD
 import Data.ByteString.Lazy as L
 import Encoding as E
+import Data.List.Split
 
 --For cryptopals
 challenge28 :: IO String
-challenge28 = return $ show $ preprocess $ E.toBytes ""
+challenge28 = return $ show (chunk512 $ preprocess $ B.replicate 56 0)
 
 h0 = 0x67452301
 h1 = 0xEFCDAB89
@@ -33,8 +34,10 @@ prepadding = \x -> div (mod (448 - x) 512) 8
 preprocess :: B.ByteString -> B.ByteString
 preprocess inp = B.concat [B.snoc inp 0x80, zwords $ prepadding (ml+8), len64 inp]
                     where ml = mlen inp
-                        
+                   
+chunk512 :: B.ByteString -> [B.ByteString]     
+chunk512 inp = Prelude.map B.pack (chunk 64 $ B.unpack inp)
 
-
---TODO: Addition mod 32
-modadd = undefined
+--TODO: This may be useful: https://mail.haskell.org/pipermail/beginners/2010-October/005571.html
+chunk32 :: B.ByteString -> [Word32]
+chunk32 inp = B.unpack inp
